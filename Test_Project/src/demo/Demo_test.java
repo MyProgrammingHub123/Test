6fae764055cc1ebabe00934a4364df5d7902136c
erum <erum@10.37.129.2>
2019-01-13 22:14:11 -0500

    adding youtube link

--- Test_Project/src/demo/Demo_test.java
+++ Test_Project/src/demo/Demo_test.java
@@ -4,7 +4,8 @@ public class Demo_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("My code");
+		//(Watch to learn how to configure in eclipse)https://www.youtube.com/watch?v=LPT7v69guVY
+		System.out.println("My code");
         System.out.println("updating My code");
 	}
 
